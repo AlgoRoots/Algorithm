187c3268de42d7d45917db1fef11e2e77369bf50
algoroot <algoroot524@gmail.com>
2022-04-14 16:53:03 +0900

    220414

--- Programmers/lv2/올바른 괄호.js
+++ Programmers/lv2/올바른 괄호.js
@@ -1,6 +1,5 @@
 function solution(s) {
   let arr = s.split("");
-  let stack = [];
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "(") {
--- Programmers/lv2/최댓값과 최솟값.js
+++ Programmers/lv2/최댓값과 최솟값.js
@@ -5,6 +5,7 @@ function solution(s) {
   answer.push(sorted.pop());
   return answer.join(" ");
 }
+
 console.log(solution("1 2 3 4")); // 	"1 4"
 console.log(solution("-1 -2 -3 -4")); // 	"-4 -1"
 console.log(solution("-1 -1")); // "-1 -1"
